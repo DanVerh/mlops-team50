69d3776a478f64147579799992e387e24179d29d
Daniil Verkhutin <92340480+DanVerh@users.noreply.github.com>
2023-11-11 04:37:13 +0200

    Update test.py

--- ray/test.py
+++ ray/test.py
@@ -4,22 +4,38 @@ import time
 
 import ray
 
+@ray.remote
+class Actor:
+    def __init__(self):
+        pass
+
+    def get_cluster_resources(self):
+        print('''This cluster consists of
+            {} nodes in total
+            {} CPU resources in total
+        '''.format(len(ray.nodes()), ray.cluster_resources()))
+
+    def task_def(self):
+        time.sleep(0.001)
+        # Return IP address.
+        return socket.gethostbyname(socket.gethostname())
+
+    def run_task(self):
+        print('''TASK
+              ----------------''')
+        object_ids = [self.task_def.remote() for _ in range(10000)]
+        ip_addresses = ray.get(object_ids)
+
+        print('Tasks executed')
+        for ip_address, num_tasks in Counter(ip_addresses).items():
+            print('    {} tasks on {}'.format(num_tasks, ip_address))
+
 ray.init()
 
-print('''This cluster consists of
-    {} nodes in total
-    {} CPU resources in total
-'''.format(len(ray.nodes()), ray.cluster_resources()['CPU']))
+# Create actors with different resource demands
+actor1 = Actor.options(num_cpus=2).remote()
+actor2 = Actor.options(num_gpus=1).remote()
 
-@ray.remote
-def f():
-    time.sleep(0.001)
-    # Return IP address.
-    return socket.gethostbyname(socket.gethostname())
-
-object_ids = [f.remote() for _ in range(10000)]
-ip_addresses = ray.get(object_ids)
-
-print('Tasks executed')
-for ip_address, num_tasks in Counter(ip_addresses).items():
-    print('    {} tasks on {}'.format(num_tasks, ip_address))
+# Run tasks on different nodes
+result1 = ray.get(actor1.run_task.remote())
+result2 = ray.get(actor2.run_task.remote())
